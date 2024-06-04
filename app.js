c7bc0968a702caeab3cbb7288faa8018c0dc3bef
Alix Karpinski <me@alexkarpinski.com>
2024-06-04 12:56:04 -0400

    wip: applyDose function and a lot of prettier auto-formatting

--- app.js
+++ app.js
@@ -1,71 +1,69 @@
-
 // Defaults for items:
-const NOSOUND_ITEM = "It makes no sound."
-const NOSMELL_ITEM = "It has no distinct smell."
+const NOSOUND_ITEM = "It makes no sound.";
+const NOSMELL_ITEM = "It has no distinct smell.";
 
 const DEFAULT_ROOM_SENSES = {
-    "see": "You see nothing.",
-    "hear": "You hear nothing.",
-    "touch": "You feel nothing but whatever is below your feet.",
-    "smell": "You detect no distinct smell.",
-    "taste": "You detect no distinct taste."
+    see: "You see nothing.",
+    hear: "You hear nothing.",
+    touch: "You feel nothing but whatever is below your feet.",
+    smell: "You detect no distinct smell.",
+    taste: "You detect no distinct taste.",
 };
 const START_LOCATION = "outside";
 const MOVE_VERBS = ["go", "exit", "move"];
 const CARDINAL_DIRECTIONS = ["north", "south", "east", "west"];
 
-
 const PERMANENT = {
-    "items": {
-        "monolith1": {
-            "moveable": false,
-            "writing": "not a place of honor",
-            "sense": {
-                "see": "A gray stone monolith, twice your height, with writing on it.",
-                "touch": "It is cold and smooth",
-                "taste": "Stony and mineral-like."
+    items: {
+        monolith1: {
+            moveable: false,
+            writing: "not a place of honor",
+            sense: {
+                see: "A gray stone monolith, twice your height, with writing on it.",
+                touch: "It is cold and smooth",
+                taste: "Stony and mineral-like.",
             },
-            "location": "outside",
-            "passive": {
-                "see": "a gray stone monolith"
-            }
+            location: "outside",
+            passive: {
+                see: "a gray stone monolith",
+            },
         },
-        "berm1": {
-            "moveable": false,
-            "sense": {},
-            "location": "outside",
-            "passive": {
-                "see": "a slope of earth rising above your head"
-            }
-        }
+        berm1: {
+            moveable: false,
+            sense: {},
+            location: "outside",
+            passive: {
+                see: "a slope of earth rising above your head",
+            },
+        },
     },
-    "rooms": {
-        "outside": {
-            "exits": {
-                "west": "information center",
-                "berm": "outside",
-                "information center": "information center"
+    rooms: {
+        outside: {
+            exits: {
+                west: "information center",
+                berm: "outside",
+                "information center": "information center",
             },
             // Rads per second
-            "rad_rate": 0,
-            "senses": {},
+            rad_rate: 0,
+            senses: {},
         },
         "information center": {
-            "exits": {
-                "east": "outside",
-                "outside": "outside",
-                "west": "hot cell",
-                "door": "hot cell"
+            exits: {
+                east: "outside",
+                outside: "outside",
+                west: "hot cell",
+                door: "hot cell",
             },
-            "rad_rate": 0.1,
-            "senses": {},
+            rad_rate: 0.1,
+            senses: {},
         },
         "hot cell": {
-            "exits": { "east": "information center" },
-            "rad_rate": 1,
-            "senses": {},
-        }
-    }
+            exits: { east: "information center" },
+            rad_rate: 1,
+            senses: {},
+        },
+    },
 };
 
 class Room {
@@ -79,7 +77,7 @@ class Room {
 
         this.exits = static_room.exits;
 
-        this.items = {} // ID to description
+        this.items = {}; // ID to description
         if (saved !== null && saved !== undefined) {
             for (const [itemid, item] of saved?.items?.entries) {
                 if (item.location === roomid) {
@@ -101,14 +99,17 @@ class Player {
     constructor(saved) {
         this.location = saved?.player?.location ?? START_LOCATION;
         this.dosage = saved?.player?.dosage ?? 0;
+        this.damage = saved?.player?.damage ?? 0;
+        // always set the last dose timer to the current time so that players do not accumulate dosage while the game is closed
+        this.lastDoseTimestamp = Date.now();
 
         // Knowledge is a set of known words.
         // We don't (yet) keep a full translation table, in either direction;
         // we just obfuscate unknown words.
-        this.knowledge = saved?.player?.knowledge ?? new Set(["place", "honor"]);
+        this.knowledge =
+            saved?.player?.knowledge ?? new Set(["place", "honor"]);
     }
-};
-
+}
 
 class State {
     constructor(permanent, saved) {
@@ -123,13 +124,13 @@ class State {
         const main = document.getElementsByTagName("main").item(0);
 
         this.presentation = document.createElement("p");
-        this.presentation.id = "presentation"
+        this.presentation.id = "presentation";
         main.appendChild(this.presentation);
 
         const form = document.createElement("form");
         form.addEventListener("submit", (ev) => {
-            ev.preventDefault()
-            this.act()
+            ev.preventDefault();
+            this.act();
         });
         main.appendChild(form);
 
@@ -149,14 +150,16 @@ class State {
     }
 
     render(error) {
-        console.log(this)
+        console.log(this);
         const room = this.currentRoom();
         const text = `
 You are at: ${this.player.location}.
 
-${Object.keys(DEFAULT_ROOM_SENSES).map(sense => {
-    return this.renderPassiveSense(sense);
-}).join("\n")}
+${Object.keys(DEFAULT_ROOM_SENSES)
+    .map((sense) => {
+        return this.renderPassiveSense(sense);
+    })
+    .join("\n")}
 //
 // TODO: Add health indicator here
 
@@ -165,7 +168,7 @@ ${error || ""}
 
         // TODO: Trash text
         // TODO Display items
-        // TODO include 
+        // TODO include
 
         if (!error) {
             this.textin.value = "";
@@ -176,12 +179,14 @@ ${error || ""}
     renderPassiveSense(sense) {
         const room = this.currentRoom();
         let senses = [room.senses[sense]];
-        const itemPassives = Object.entries(room.items).map(([_itemId, item]) => {
-            return item.passive[sense];
-        });
-        senses = [ ...senses, ...itemPassives ].filter(sense => sense);
+        const itemPassives = Object.entries(room.items).map(
+            ([_itemId, item]) => {
+                return item.passive[sense];
+            }
+        );
+        senses = [...senses, ...itemPassives].filter((sense) => sense);
         if (senses.length > 1) {
-            senses[senses.length - 1] = `and ${senses[senses.length - 1]}`
+            senses[senses.length - 1] = `and ${senses[senses.length - 1]}`;
         }
         if (senses.length === 0) {
             return DEFAULT_ROOM_SENSES[sense];
@@ -197,11 +202,13 @@ ${error || ""}
         // TODO: Parse user input and change the current state, then...
         //
         //
+        applyDose();
         const tokenizedAction = this.textin.value.split(" ");
         let error = `I don't know ${tokenizedAction[0]}`;
         if (MOVE_VERBS.includes(tokenizedAction[0])) {
             error = this.movePlayer(tokenizedAction.slice(1).join(" "));
         }
+        applyDose();
         this.render(error);
     }
 
@@ -213,8 +220,23 @@ ${error || ""}
         }
         return `Cannot move to ${direction}`;
     }
+
+    applyDose() {
+        // cancel any existing dose callback timer
+
+        // record last time dosage was applied
+        let now = Date.now();
+        let timeSinceLastDosage = now - this.player.lastDoseTimestamp;
+        this.player.lastDoseTimestamp = now;
+        // update dosage based on time since last dose
+        this.player.dosage +=
+            (timeSinceLastDosage * this.currentRoom().rad_rate) / 1000;
+        // apply dosage to damage
+        this.player.damage += (timeSinceLastDosage * this.player.dosage) / 1000;
+
+        // compute time until next threshhold at current player.dosage
+        // set dosage callback and store timer on state
+    }
 }
 
-
-
-const state = new State(PERMANENT, /*saved = */undefined);
+const state = new State(PERMANENT, /*saved = */ undefined);
